6885e77a900b15f6d8c12055c770ed9e097c8b49
gappc <gapp.christian@gmail.com>
2021-12-14 23:43:33 +0100

    Add Jenkins config files

--- infrastructure/Jenkinsfile-Prod.groovy
+++ infrastructure/Jenkinsfile-Prod.groovy
@@ -3,14 +3,15 @@ pipeline {
 
     environment {
         DOCKER_PROJECT_NAME = "odh-broker-mqtt"
-        DOCKER_IMAGE = "eclipse-mosquitto"
-        DOCKER_TAG = "2.0.12"
+        DOCKER_IMAGE = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/odh-broker-mqtt"
+        DOCKER_TAG = "test-$BUILD_NUMBER"
 
         SERVER_PORT = "1883"
 
-        MOSQUITTO_CONF = "./docker/mosquitto/mosquitto.conf"
-        MOSQUITTO_INCLUDE_DIR = "./docker/mosquitto/conf.d"
-        MOSQUITTO_PORT = "1883"
+        AUTH_USER = credentials('odh-broker-mqtt-test-auth-username')
+        AUTH_PASSWORD = credentials('odh-broker-mqtt-test-auth-password')
+        TOPIC_ANONYMOUS = credentials('odh-broker-mqtt-test-topic-anonymous')
+        TOPIC_RESTRICTED = credentials('odh-broker-mqtt-test-topic-restricted')
     }
 
     stages {
@@ -26,9 +27,10 @@ pipeline {
 
                     echo 'SERVER_PORT=${SERVER_PORT}' >> .env
 
-                    echo 'MOSQUITTO_CONF=${MOSQUITTO_CONF}' >> .env
-                    echo 'MOSQUITTO_INCLUDE_DIR=${MOSQUITTO_INCLUDE_DIR}' >> .env
-                    echo 'MOSQUITTO_PORT=${MOSQUITTO_PORT}' >> .env
+                    echo 'AUTH_USER=${AUTH_USER}' >> .env
+                    echo 'AUTH_PASSWORD=${AUTH_PASSWORD}' >> .env
+                    echo 'TOPIC_ANONYMOUS=${TOPIC_ANONYMOUS}' >> .env
+                    echo 'TOPIC_RESTRICTED=${TOPIC_RESTRICTED}' >> .env
                 """
             }
         }
@@ -51,7 +53,8 @@ pipeline {
             steps {
                sshagent(['jenkins-ssh-key']) {
                     sh """
-                        (ls)
+                        (cd infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
+                        (cd infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER}")
                     """
                 }
             }
